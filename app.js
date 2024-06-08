4e863d997d3ed5514af7ff3d629aea08bef21593
Neerav Sood <165214507+NeeravSood@users.noreply.github.com>
2024-06-08 17:06:58 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Suggestions from './Suggestions';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Real-time Suggestions</h1>
+        <Suggestions />
+      </header>
+    </div>
+  );
+}
+
+export default App;
